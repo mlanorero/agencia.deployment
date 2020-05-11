45e8274f0da97e6c3c695d21248b2f2d011ae45d
mlanorero <mlanorero@gmail.com>
2020-05-11 10:44:49 -0400

    Mi primer Commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-require('dotenv').config({ path: 'viriables.env' })
+require('dotenv').config({ path: 'variables.env' })
 
 module.exports = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER,  process.env.BD_PASS, {
     host: process.env.BD_HOST,
